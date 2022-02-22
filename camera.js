4963ed629f4ca85b4b96a15bdfb1a11c898c0382
Damien Seguin <dmnsgn@gmail.com>
2022-02-22 18:26:42 +0000

    feat: add generic camera interface

---
+++ camera.js
@@ -0,0 +1,29 @@
+import { mat4 } from "pex-math";
+
+export default class Camera {
+  // Static getter to get different mat for each instances
+  static get DEFAULT_OPTIONS() {
+    return {
+      projectionMatrix: mat4.create(),
+      invViewMatrix: mat4.create(),
+      viewMatrix: mat4.create(),
+      position: [0, 0, 3],
+      target: [0, 0, 0],
+      up: [0, 1, 0],
+      aspect: 1,
+      near: 0.1,
+      far: 100,
+      view: null,
+    };
+  }
+
+  set(opts) {
+    Object.assign(this, opts);
+
+    if (opts.position || opts.target || opts.up) {
+      mat4.lookAt(this.viewMatrix, this.position, this.target, this.up);
+      mat4.set(this.invViewMatrix, this.viewMatrix);
+      mat4.invert(this.invViewMatrix);
+    }
+  }
+}
